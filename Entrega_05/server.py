7e72b5fb8d99d89dd2ad6bb9933dbc2aed7e912f
lucassa3 <lucassa3@al.insper.edu.br>
2018-12-04 11:47:02 -0200

    5

---
+++ Entrega_05/server.py
@@ -0,0 +1,21 @@
+from flask import render_template, Flask
+import cv2
+
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+
+@app.route('/')
+def index():
+    gen()
+    return render_template('index.html')
+
+
+def gen():
+        _, frame = camera.read()
+        cv2.imwrite('static/images/frame.png', frame)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
